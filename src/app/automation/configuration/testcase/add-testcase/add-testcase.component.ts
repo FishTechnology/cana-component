c439b30baad9352f97de86007c069cebd14fc1f3
sajanmje27 <sajanmje82@gmail.com>
2022-01-15 17:26:00 -0500

    fixed test plan mapping

--- src/app/automation/configuration/testcase/add-testcase/add-testcase.component.ts
+++ src/app/automation/configuration/testcase/add-testcase/add-testcase.component.ts
@@ -1,10 +1,18 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import {
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 import { CustomerService } from 'src/app/commons/customer/customer.service';
 import { CustomerDetail } from 'src/app/commons/customer/models/CustomerDetail';
+import { SnackbarService } from 'src/app/commons/snackbar/snackbar.service';
 import { TestPlanModel } from '../../testplan/models/TestPlanModel';
 import { TestplanService } from '../../testplan/testplan.service';
 import { TestCaseModel } from '../models/TestCaseModel';
+import { TestCaseOrderModel } from '../models/TestCaseOrderModel';
+import { UpdateTestCaseOrderModel } from '../models/UpdateTestCaseOrderModel';
 import { TestCaseService } from '../testcase.service';
 
 @Component({
@@ -16,16 +24,39 @@ export class AddTestcaseComponent implements OnInit {
   customerDetail!: CustomerDetail;
   testPlanModels!: TestPlanModel[];
   testCaseModels!: TestCaseModel[];
+  newTestCaseModel!: TestCaseModel;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  testCaseId!: string;
+  selectedTestPlan!: TestPlanModel;
+  isShowSaveAction: boolean = false;
 
   constructor(
     private testCaseService: TestCaseService,
     private testPlanService: TestplanService,
-    private customerService: CustomerService
+    private customerService: CustomerService,
+    private snackbarService: SnackbarService,
+    private router: ActivatedRoute
   ) {
+    this.router.params.subscribe((params) => {
+      this.testCaseId = params['testcaseid'];
+      this.getTestCaseByTestCaseId();
+    });
     this.customerService.getUserDetail().subscribe((res) => {
       this.customerDetail = res;
       this.getTestPlanByUserId();
     });
+  }
+
+  getTestCaseByTestCaseId(): void {
+    this.testCaseService.getTestCaseById(this.testCaseId).subscribe(
+      (res) => {
+        this.newTestCaseModel = res;
+      },
+      (err) => {
+        this.snackbarService.openSnackBar('Error loading test case by id');
+      }
+    );
   }
 
   ngOnInit(): void {}
@@ -35,12 +66,12 @@ export class AddTestcaseComponent implements OnInit {
   getTestPlanByUserId(): void {
     this.testPlanService
       .getTestPlansByUserId(this.customerDetail.userId)
-      .subscribe(
-        (res) => {
+      .subscribe({
+        next: (res) => {
           this.testPlanModels = res;
         },
-        (err) => {}
-      );
+        error: (err) => {},
+      });
   }
 
   getTestCaseByTestPlanId(testPlanId: string): void {
@@ -48,7 +79,9 @@ export class AddTestcaseComponent implements OnInit {
       (res) => {
         this.testCaseModels = res;
       },
-      (err) => {}
+      (err) => {
+        this.snackbarService.openSnackBar('Error loading test case');
+      }
     );
   }
 
@@ -58,5 +91,51 @@ export class AddTestcaseComponent implements OnInit {
       event.previousIndex,
       event.currentIndex
     );
+  }
+
+  testPlanClick(testPlanModel: TestPlanModel): void {
+    this.selectedTestPlan = testPlanModel;
+    this.testCaseService.getTestCaseByTestPlanId(testPlanModel.id).subscribe(
+      (res) => {
+        this.testCaseModels = res;
+        this.isShowSaveAction = true;
+      },
+      (err) => {
+        this.snackbarService.openSnackBar('Error loading test plan');
+      }
+    );
+  }
+
+  addTestCase(): void {
+    this.testCaseModels.push(this.newTestCaseModel);
+  }
+
+  saveTestCase(): void {
+    let testCaseOrderModels: TestCaseOrderModel[] = [];
+
+    this.testCaseModels.forEach((testCaseModel, index) => {
+      let testCaseOrderModel: TestCaseOrderModel = {
+        testCaseId: testCaseModel.id,
+        oldExecutionOrder: testCaseModel.executionOrder,
+        currentExecutionOrder: index + 1,
+      };
+      testCaseOrderModels.push(testCaseOrderModel);
+    });
+
+    let updateTestCaseOrderModel: UpdateTestCaseOrderModel = {
+      userId: this.customerDetail.userId,
+      testcaseOrderModels: testCaseOrderModels,
+    };
+
+    this.testCaseService
+      .updateTestCaseOrder(this.selectedTestPlan.id, updateTestCaseOrderModel)
+      .subscribe({
+        next: (res) => {
+          this.snackbarService.openSnackBar('Successfully updated order');
+        },
+        error: (err) => {
+          this.snackbarService.openSnackBar('Error while updating order');
+        },
+      });
   }
 }
